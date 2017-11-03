4db3802524f537a479750ee8761170345e04a49a
Heuster <32402568+Heuster@users.noreply.github.com>
2017-11-03 14:15:49 -0700

    MineSweeper
    11/3/2017

---
+++ MinesweeperBoard2.java
@@ -0,0 +1,79 @@
+
+/**
+ * Write a description of class Minesweeper here.
+ * 
+ * @author Danny Heu
+ * @version 10.31.2017
+ */
+import java.lang.Math;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class MinesweeperBoard2{
+    Cell[] board;
+    int rows;
+    int columns;
+    int index;
+    public MinesweeperBoard2(int rows, int columns){
+        //Put the constructor here.
+        this.rows = rows;
+        this.columns = columns;
+        board = new Cell[rows*columns];
+        //These pieces are for the GUI.
+        JFrame frame = new JFrame();
+        frame.add(addCells());
+        
+        frame.pack();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    
+    public MinesweeperBoard2(){
+        this(10,10);
+    }
+    
+    public void addBombs(int bombs) {//throws Exception{
+        // enter number of bombs, bombs placed in random on the board
+        boolean createBomb;
+        for (int i = 0; i < index; i++){
+            createBomb = false;
+            while (!createBomb) {
+                int randInt = 0;
+            }
+        }
+    }
+
+    public void checkBombs(){
+        
+    }
+    
+    public void addNums(){
+      
+    }
+    /**This method is used for testing and will be deleted if using the GUI.
+     *  It is still required for all students.
+     */
+    public void printBoard(){
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                if (board[index].getValue() == -1){
+                    System.out.print("X");
+                } else {
+                    System.out.print(board[index].getValue() + " ");
+                }
+           }
+           System.out.println();
+        }
+    }
+    
+    public JPanel addCells(){
+        JPanel panel = new JPanel(new GridLayout(rows,columns));
+        for(int i = 0; i< rows*columns; i++){
+                board[i]= new Cell();
+                panel.add(board[i].getButton());
+        }
+        return panel;
+    }
+
+}
